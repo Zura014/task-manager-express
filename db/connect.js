808a57fb94a41af2755f45b369967fc02731f281
Zura014 <zuragagnidze50@gmail.com>
2024-12-25 22:33:08 +0400

    add database connection and update mongoose version

--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,14 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://zuragagnidze50:<db_password>@taskmanagerexpress.ad4da.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerExpress";
+  "mongodb+srv://zuragagnidze50:fagLdp9gi1Ruk13G@taskmanagerexpress.ad4da.mongodb.net/task-manager?retryWrites=true&w=majority&appName=TaskManagerExpress";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("CONNECTED TO DATABASE..."))
+  .catch((err) => console.log("ERROR: ", err));
