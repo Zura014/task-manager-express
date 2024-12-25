bf55915441950e88a35ed2a5e3dc47564c9da9fd
Zura014 <zuragagnidze50@gmail.com>
2024-12-25 22:59:57 +0400

    refactor database connection to use environment variable and async initialization

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://zuragagnidze50:fagLdp9gi1Ruk13G@taskmanagerexpress.ad4da.mongodb.net/task-manager?retryWrites=true&w=majority&appName=TaskManagerExpress";
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-  })
-  .then(() => console.log("CONNECTED TO DATABASE..."))
-  .catch((err) => console.log("ERROR: ", err));
+  });
+};
+
+module.exports = connectDB;
